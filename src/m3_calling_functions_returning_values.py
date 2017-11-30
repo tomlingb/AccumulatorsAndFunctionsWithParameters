a17d4fca8fc5a79614c42949abb0d5e9283e7413
tomlingb <tomlingb@rose-hulman.edu>
2017-11-30 10:54:51 -0500

    TODO 4

--- src/m3_calling_functions_returning_values.py
+++ src/m3_calling_functions_returning_values.py
@@ -174,8 +174,13 @@ def digits_in_cube(n):
       If the integer (n) is 5    (so n cubed is 125),
       this function returns (1 + 2 + 5), which is 8.
     """
+
+    n = n**3
+    answer = sum_of_digits(n)
+    return answer
+
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
