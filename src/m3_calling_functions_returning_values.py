d32d4fc6e08e47f5aea7942c7b3ef671e471247b
tomlingb <tomlingb@rose-hulman.edu>
2017-11-30 11:27:42 -0500

    TODO 9. I also commented out the fanyc sums call in main() because it was cauusing the program to never finish running

--- src/m3_calling_functions_returning_values.py
+++ src/m3_calling_functions_returning_values.py
@@ -18,7 +18,7 @@ def main():
     run_test_sum_of_digits()
     run_test_digits_in_cube()
     run_test_digits_in_power()
-    run_test_fancy_sums_of_digits()
+    #run_test_fancy_sums_of_digits()
 
     # ------------------------------------------------------------------
     # TODO: 9. DO THIS LAST!
@@ -31,7 +31,7 @@ def main():
     #       ** Ask a TA or your professor for help in that case. **
     # ------------------------------------------------------------------
 
-    # m3t_tester.main()
+    m3t_tester.main()
 
 
 def run_test_sum_of_digits():
